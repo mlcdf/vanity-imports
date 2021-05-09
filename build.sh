948d64a63270d79e376ec49118f5f566f06b5ebe
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2021-05-09 13:02:45 +0200

    misc

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 set -e
 
-# download latest release of dyndns
+# download latest release of vanity-imports
 LOCATION=$(curl -s https://api.github.com/repos/mlcdf/vanity-imports/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4)
 echo $LOCATION
 curl -L "${LOCATION}" -o vanity-imports
@@ -13,3 +13,5 @@ chmod +x ./vanity-imports
 
 # Build the pages using .vanity-imports.toml to ./dist
 ./vanity-imports
+
+# => https://go.mlcdf.fr
