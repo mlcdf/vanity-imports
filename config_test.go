da2403da43e9a8779f546d28113a8a3607396312
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2021-05-11 01:27:10 +0200

    add tests

---
+++ config_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"path"
+	"strings"
+	"testing"
+)
+
+func TestInvalidConfig(t *testing.T) {
+	testCases := []struct {
+		path    string
+		message string
+	}{
+		{
+			path:    "missing-title.toml",
+			message: "index.title",
+		},
+		{
+			path:    "missing-domain.toml",
+			message: "domain",
+		},
+		{
+			path:    "missing-repo.toml",
+			message: "repo",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.path, func(t *testing.T) {
+			_, err := newConfig(path.Join("fixtures", tC.path))
+			if err != nil && !strings.Contains(err.Error(), tC.message) {
+				t.Errorf("'%s' does not contains '%s'", err, tC.message)
+			}
+		})
+	}
+}
+func TestValidConfig(t *testing.T) {
+	_, err := newConfig(path.Join("fixtures", "valid.toml"))
+	if err != nil {
+		t.Error(err)
+	}
+}
